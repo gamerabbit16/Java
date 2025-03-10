29f6d316b717b84c2f23fc70431dded7acee8f8e
Gamerabbit <jordapaul16@gmail.com>
2025-03-10 19:49:01 +0100

    implement code2 and code3 classes with main methods and initial logic

--- code2.java
+++ code2.java
@@ -1,6 +1,10 @@
 public class code2 {
     public static void main(String[] args) {
-        
-        
+        for (int i = 0; true; i++){
+            if (i % 2 == 0 && i % 3 == 0) {
+              break;
+            }
+    
+          }
     }
 }
---
+++ code3.java
@@ -0,0 +1,12 @@
+public class code3 {    
+    public static void main(String[] args) {
+        for (int i = 0; i < 52; i++){
+            if (i == 32){
+              System.out.println("wrong card at " + i);
+              break;
+            } else {
+            System.out.println("card " + i);
+            }
+          }
+    }
+}
