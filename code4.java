9bdb4ab9bad7a3e22e760f4bd2dd28de9d28762b
Gamerabbit <jordapaul16@gmail.com>
2025-03-29 23:15:09 +0100

    ajouter la classe code4 avec une méthode main qui imprime des messages

---
+++ code4.java
@@ -0,0 +1,8 @@
+public class code4 {
+    public static void main(String[] args) {
+        // This program prints the numbers from 1 to 50
+        System.out.println("hewo");
+        String message = "your mom";
+        System.out.println(message);
+    }
+}
